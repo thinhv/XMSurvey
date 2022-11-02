b763faed5e111e836376358cc724d95ab5370554
Thinh Vo <ducthinh2410@gmail.com>
2022-11-02 20:51:42 +0200

    Initial Commit

---
+++ XMSurvey/XMSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMSurveyApp.swift
+//  XMSurvey
+//
+//  Created by Thinh on 2.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct XMSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
